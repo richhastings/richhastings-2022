1068e3b086f3b6120abf82a6c1ce0d93793a97e4
Rich Hastings <rich.hastings@clock.co.uk>
2022-01-14 16:15:46 +0000

    initial commit

---
+++ src/component/Home/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import GitHubSVG from '../../asset/icon/github.svg'
+import TwitterSVG from '../../asset/icon/twitter.svg'
+import InstagramSVG from '../../asset/icon/instagram.svg'
+
+import styles from './Home.module.scss'
+
+const Home = () => (
+  <>
+    <div className={styles.Content}>
+      <h1>Hi, I'm Rich.</h1>
+      <h2>An experienced Front-end Engineer from Sherwood Forest, UK.</h2>
+    </div>
+    <div className={styles.Social}>
+      <a href="https://github.com/richhastings">
+        <GitHubSVG />
+      </a>
+      <a href="https://twitter.com/sheffieldcoder">
+        <TwitterSVG />
+      </a>
+      <a href="https://www.instagram.com/thesheffieldcoder/">
+        <InstagramSVG />
+      </a>
+    </div>
+  </>
+)
+
+export default Home
---
+++ src/component/Main/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import styles from './Main.module.scss'
+
+const Main = ({ children }) => <main className={styles.Main}>{children}</main>
+
+export default Main
