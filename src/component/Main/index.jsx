98940528b6b70f1f57dfc1e445227d18cee8a85d
Rich Hastings <rich.hastings@clock.co.uk>
2022-01-14 16:40:54 +0000

    trigger deployment

--- src/component/Main/index.jsx
+++ src/component/Main/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import styles from './Main.module.scss'
 
+//TRIGGER deployment
+
 const Main = ({ children }) => <main className={styles.Main}>{children}</main>
 
 export default Main
